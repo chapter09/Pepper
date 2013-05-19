546c12d1971d9d8247074d783d8102e3cc4bc093
Lou Jian <gsj987@gmail.com>
2013-05-19 12:32:37 +0800

    基本完成recipe页

--- static/js/modules/recipe.js
+++ static/js/modules/recipe.js
@@ -1,9 +1,12 @@
 (function(pepper, Recipe) {
  
   var Common = pepper.module('common');
+  var Paper = pepper.module('paper');
+  var Comment = pepper.module('comment');
  
   Recipe.Model = Backbone.Model.extend({
     idAttribute: '_id',
+    urlRoot: '/recipes',
     dataAdapter: function(){
 		  return {
 		    'item_title': this.get('title'),
@@ -56,6 +59,92 @@
         
     }
   });
+  
+  
+  Recipe.Page = Backbone.View.extend({
+    el: "#main-container",
+    
+    render: function(){
+      
+      data = {
+        title: this.model.get('title'),
+        description: this.model.get('description'),
+        forks: this.model.get('forks').length,
+        watches: this.model.get('watches').length,
+        inwatch: (app.current_user && 
+            this.model.get('_id') in app.current_user.watches)
+      };
+      
+      _papers = this.model.get('_papers');
+      paperList = new Paper.List(_papers);
+      
+      _comments = this.model.get('_comments');
+      comments = new Comment.List(_comments);
+      
+      $(this.el).html(ich.recipeViewPage(data));
+      
+      $('.watch').click(this.watch);
+      $('.fork').click(this.fork);
+      
+      
+      paperListView = new Paper.ListView({
+        el: '#paper-list',
+        collection: paperList
+      });
+      
+      paperListView.render();
+      
+      
+      cw = new Comment.InputView({
+        modelObject: this.model,
+        collection: comments,
+        el: '.my-comment-list',
+      });
+      
+      cw.render();
+      
+      cv = new Comment.ListView({
+        tagName: "div",
+        id: "comment-list",
+        collection: comments
+      });
+      
+      $('.comment-list-wrapper', this.el).html(cv.render().el);
+      
+      
+    }
+  });
+  
+  Recipe.Router = Backbone.Router.extend({
+    initialize: function(){
+    },
+     
+     routes: {
+       "recipe/:recipe_id": "recipe",
+     },
+    
+     recipe: function(recipe_id){
+       app.render_widgets();
+       
+       model = new Recipe.Model({_id: recipe_id});
+       
+       $("#loading-animtion").show();
+       
+       model.fetch({success:function(model, success){
+         
+         view = new Recipe.Page({
+           model:model
+         });
+         
+         
+         view.render();
+         $('#loading-animtion').hide();
+         
+       }});
+       
+     }
+   });
+  
   
  
  // Shorthands 
